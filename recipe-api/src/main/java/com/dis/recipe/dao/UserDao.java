a62499354f34a62b5d1e0a06ae4806877b5a5de9
August Lorenzen <augustplorenzen@icloud.com>
2024-06-08 22:33:19 +0200

    Update UserDao.java

--- recipe-api/src/main/java/com/dis/recipe/dao/UserDao.java
+++ recipe-api/src/main/java/com/dis/recipe/dao/UserDao.java
@@ -10,10 +10,10 @@ import java.util.List;
 
 public interface UserDao extends JpaRepository<User, String>{
 
-    @Query(value = "SELECT * FROM Userdis WHERE username = ?1 OR email = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM Userdis WHERE LOWER(username) = LOWER(?1) OR LOWER(email) = LOWER(?2)", nativeQuery = true)
     List<User> findUserByUsernameOrEmail(String username, String email);
 
-    @Query(value = "SELECT * FROM Userdis WHERE username = ?1 AND password = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM Userdis WHERE LOWER(username) = LOWER(?1) AND password = ?2", nativeQuery = true)
     List<User> findUserByUsernameAndPassword(String username, String password);
 
     @Modifying
@@ -21,7 +21,7 @@ public interface UserDao extends JpaRepository<User, String>{
     @Query(value = "INSERT INTO Userdis (id, username, password, email) VALUES (?1, ?2, ?3, ?4)", nativeQuery = true)
     void insert(String id, String username, String password, String email);
 
-    @Query(value = "SELECT * FROM Userdis WHERE id = ?1 AND username = ?2 AND password = ?3", nativeQuery = true)
+    @Query(value = "SELECT * FROM Userdis WHERE id = ?1 AND LOWER(username) = LOWER(?2) AND password = ?3", nativeQuery = true)
     List<User> findUserByIdAndUsernameAndPassword(String id, String username, String password);
 
     @Modifying
