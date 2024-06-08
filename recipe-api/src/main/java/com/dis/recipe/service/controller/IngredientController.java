b52ee9eab4599032d981b51b3353ed8abbd2d172
Mikail Oruc <mikail.oruc@live.dk>
2024-06-08 17:22:40 +0200

    added ingredients flow

--- recipe-api/src/main/java/com/dis/recipe/service/controller/IngredientController.java
+++ recipe-api/src/main/java/com/dis/recipe/service/controller/IngredientController.java
@@ -2,6 +2,8 @@ package com.dis.recipe.service.controller;
 
 import com.dis.recipe.service.IngredientService;
 import com.dis.recipe.dto.IngredientInfo;
+import com.dis.recipe.dto.RecipeInfo;
+
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.HttpServletRequest;
 import java.math.BigInteger;
+import java.util.List;
 
 @Slf4j
 @RequestMapping("ingredient")
@@ -17,10 +20,31 @@ import java.math.BigInteger;
 public class IngredientController {
 	private final IngredientService ingredientService;
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<IngredientInfo> getAll() {
+        return ingredientService.getAllIngredients();
+    }
+
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public IngredientInfo getIngredientById(@PathVariable String id) {
+		IngredientInfo ingredientInfo = ingredientService.getIngredientById(id);
+		return ingredientInfo;
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteIngredient(@PathVariable String id) {
+		ingredientService.deleteIngredient(id);
+		return;
+	}
+
+	@PutMapping("/{id}/name")
+	public void updateIngredientName(@PathVariable String id, @RequestParam String name) {
+		ingredientService.updateIngredientName(id, name);
+		return;
+	}
+
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public int create(@RequestBody IngredientInfo source) {
-		IngredientInfo test = ingredientService.create(source);
-		System.out.println(test);
-		return 5;
+	public IngredientInfo create(@RequestBody IngredientInfo source) {
+		return ingredientService.create(source);
 	}
 }
